bc18d81a9b29256a2542b25caba40d61da8d21bd
bosOksi <boyko.oks.s@gmail.com>
2019-12-01 20:35:11 +0300

    hw2

--- src/test/java/hw2/FirstAndFinalCommonSteps.java
+++ src/test/java/hw2/FirstAndFinalCommonSteps.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class firstAndFinalCommonSteps {
+public class FirstAndFinalCommonSteps {
 
     protected WebDriver driver;
 
